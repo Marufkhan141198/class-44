75bea737c558cbaf9534776d23d81eb12478e93f
Marufkhan141198 <khannmaruf141198@gmail.com>
2025-03-30 12:03:08 +0600

    Php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+echo "<h1>This is our first class on php</h1>"
+?>
+
+<?php
+  $indexArray = [12, 23, 45, 35 ];
+
+  for($i= 0; $i<4; $i++){
+    echo $indexArray[$i]."<br>";
+  }
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-o1</title>
+</head>
+<body>
+    <h1>This is our first website<h1>
+    <?php
+    $x = 10;
+    $y = 12;
+    $sum = $x + $y;
+    echo "The sum is = ".$sum; 
+
+    ?>    
+</body>
+</html>
